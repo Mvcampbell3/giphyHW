534c54d13c9541bf7d56cd3209ec4006e89557b6
Mvcampbell3 <mvcampbell3@gmail.com>
2018-12-30 19:54:08 -0500

    added basic ajax call and button area

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # giphyHW
 
+## Version 1.01
+
+Added all needed to version 1.00
+
+### What needs to be added next
+
+- response from ajax to be added to html in gif form
+- styling to make it look nice
+- keep in mind the requirements such as
+    - ability to tell if same button was pressed or not
+    - add g rating
+    
+
 ## Version 1.00
 
 Things to add:
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,27 @@
 $(".submitBtn").on("click", function(event){
     event.preventDefault();
-    console.log("working");
-    
+    var search = $("#wordSearch").val().trim();
+    console.log(search);
+    if (search === ""){
+        alert("Please enter something into the search bar");
+        return;
+    } else {
+        console.log("working");
+        var newBtn = $("<button>").attr("class", "btn btn-primary gifName").attr("data-name", search).text(search);
+        $(".buttonArea").append(newBtn);
+        $("#wordSearch").val("");
+    }
+});
+
+$(".buttonArea").on("click", ".gifName", function(){
+    var search = $(this).attr("data-name");
+    console.log("this is the name of the btn " + search);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=d7kvX0voWdIu8foSOpdhIvesiVLi8dsN&q=" + search + "&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        
+    });
 })
